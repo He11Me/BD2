b0df0d2e0dd2b291390a4a82cf2abb26e4b60d05
He11Me <tasher.2018@inbox.ru>
2023-12-16 09:51:25 +0300

    Project is resdy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,49 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
+    private static final DatabaseProperties properties = PropertiesFactory.getProperties();
 
-    public Main() {
-    }
-
-    public static void main(String[] args) {
+    public void main(String[] args) {
         try {
+
             Class.forName("org.postgresql.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/training_hw_5", "postgres", "pass");
+            Connection connection = DriverManager.getConnection(properties.getUrl(), properties.getLogin(), properties.getPassword());
+
             if (connection != null) {
-                System.out.println("Connected");
-                getData(connection);
+                getData(connection, "audit_department");
             } else {
                 System.out.println("not connected");
             }
-        } catch (Exception var2) {
-            System.out.println(var2.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    private static void getData(Connection connection) {
+    public void getData(Connection connection, String table) {
         Statement statement = null;
         ResultSet result = null;
 
         try {
-            String query = "SELECT * FROM " + "audit_department";
+            String query = "SELECT * FROM " + table;
             statement = connection.createStatement();
             result = statement.executeQuery(query);
+
             System.out.print("Ревизионный номер изделия| ");
             System.out.print("Тип изделия| ");
             System.out.println("Адрес расположения");
 
-            while(result.next()) {
+            while (result.next()) {
                 System.out.print(result.getString("Ревизионный номер изделия") + "| ");
                 System.out.print(result.getString("Тип изделия") + "| ");
                 System.out.print(result.getString("Адрес расположения"));
             }
-        } catch (Exception var5) {
-            System.out.println(var5.getMessage());
-        }
 
+        } catch (Exception e) {
+            System.out.println("error");
+        }
     }
+
+
+
 }
+
---
+++ src/main/resources/console.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE training_hw_5;
+CREATE TABLE audit_department ("Ревизионный номер изделия" text, "Тип изделия" text, "Адрес расположения" text);
+
+INSERT INTO audit_department(
+    "Ревизионный номер изделия",
+    "Тип изделия",
+    "Адрес расположения"
+)
+VALUES
+    ('Xg_5124', 'Xg_5125',
+     'Компьютер', 'Удлинитель',
+     'Общага', 'Общага'
+    );
